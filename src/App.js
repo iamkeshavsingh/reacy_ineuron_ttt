3a4eb88aa68504f8b6b83e4c065c5677885fa606
Keshav Singh <iamerratic@users.noreply.github.com>
2021-05-29 21:30:24 +0530

    layout done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <Container>
+            <Row className="justify-content-center mt-4">
+                <h1>Tic Tac Toe</h1>
+            </Row>
+            <Row className="mt-4 p-3">
+                <Col md={4}>
+                    <Form>
+                        <h3>Players Information</h3>
+                        <Form.Group>
+                            <Form.Label>Player 1 Name:</Form.Label>
+                            <Form.Control type="text"></Form.Control>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Player 2 Name:</Form.Label>
+                            <Form.Control type="text"></Form.Control>
+                        </Form.Group>
+                        <Button type="submit" variant="primary">
+                            Start The Game
+                        </Button>
+                    </Form>
+                </Col>
+                <Col md={{ span: 4, offset: 4 }}>
+                    <Row className="justify-content-center">
+                        <h5>Michal's Turn</h5>
+                        <Col md={12} className="p-4 App_board">
+                            <Row className="App_board_row">
+                                <Col></Col>
+                                <Col></Col>
+                                <Col></Col>
+                            </Row>
+                            <Row className="App_board_row">
+                                <Col></Col>
+                                <Col></Col>
+                                <Col></Col>
+                            </Row>
+                            <Row className="App_board_row">
+                                <Col></Col>
+                                <Col></Col>
+                                <Col></Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+            <Row className="justify-content-center mt-4">
+                <h4>Winner is: John</h4>
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
